ceb8517ca153eeb369c2c2e944cb02a8fca59186
white-letter <121116111@qq.com>
2022-01-21 00:07:28 +0800

    wip: useFetch

--- src/components/rich/index.vue
+++ src/components/rich/index.vue
@@ -2,7 +2,7 @@
   import { onBeforeUnmount, defineProps, nextTick } from 'vue'
 
   import { Toolbar, Editor, getEditor, removeEditor } from '@wangeditor/editor-for-vue'
-  import { IDomEditor } from '@wangeditor/editor'
+  import { IDomEditor, IEditorConfig } from '@wangeditor/editor'
   import '@wangeditor/editor/dist/css/style.css'
 
   const props = defineProps({
@@ -23,7 +23,21 @@
   const editorId = `w-e-${Math.random().toString().slice(-5)}`
   const mode = 'default'
   const toolbarConfig = {}
-  const editorConfig = { placeholder: props.placeholder }
+  const editorConfig: Partial<IEditorConfig> = {
+    placeholder: props.placeholder,
+    MENU_CONF: {
+      uploadImage: {
+        // 自定义上传
+        async customUpload(file: File, insertFn: any) {
+          // file 即选中的文件
+          // 自己实现上传，并得到图片 url alt href
+          // 最后插入图片
+          // insertFn(url, alt, href)
+          console.log(file, insertFn)
+        }
+      }
+    }
+  }
 
   const existEditor = (cb: (editor: IDomEditor) => void) => {
     const editor = getEditor(editorId)
---
+++ src/hooks/utils/fetch.ts
@@ -0,0 +1,92 @@
+import { reactive, ref, unref } from 'vue'
+
+type IFetchOption = {
+  api: (params: any) => Promise<any>
+  auto?: boolean
+  params?: any
+  format?: (res: any) => any
+  onSuccess?: (data: any) => void
+  defaultValue?: any
+}
+
+export const useFetch = ({ api, params, auto = true, format, onSuccess, defaultValue = {} }: IFetchOption) => {
+  const loading = ref(false)
+  const result = ref(defaultValue)
+
+  const fetchApi = async (params: any) => {
+    loading.value = true
+
+    try {
+      const res = await api(params)
+      result.value = format ? format(res) : res
+      loading.value = false
+      const data = unref(result)
+      onSuccess?.(data)
+      return Promise.resolve(data)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+
+  if (auto) {
+    fetchApi(params)
+  }
+
+  return { fetchApi, loading, result }
+}
+
+export const useFetchList = (option: IFetchOption) => {
+  const { fetchApi: fetchList, loading, result: list } = useFetch({ ...option, defaultValue: [] })
+  return { fetchList, loading, list }
+}
+
+export const useFetchPageList = (option: IFetchOption) => {
+  const paginationChange = (cur: number, size: number) => {
+    pagination.current = cur
+    pagination.pageSize = size
+    fetchPageList(params)
+  }
+
+  const pagination = reactive({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+    pageSizeOptions: ['10', '20', '50', '100'],
+    showSizeChanger: true,
+    showLessItems: true,
+    showQuickJumper: true,
+    showTotal: (total: number, range: number[]) => `第 ${range[0]}-${range[1]} 条，总计 ${total} 条`,
+    onShowSizeChange: paginationChange,
+    onChange: paginationChange
+  })
+
+  const { auto = true, params, format, onSuccess } = option
+
+  const { fetchList, list, loading } = useFetchList({
+    ...option,
+    auto: false,
+    format: (res: any) => {
+      return format ? format(res.records) : res.records
+    },
+    onSuccess: (data: any) => {
+      pagination.total = data.total
+      onSuccess?.(data)
+    }
+  })
+
+  const fetchPageList = (params: any, reset?: boolean) => {
+    if (reset) {
+      pagination.current = 1
+    }
+
+    const { current, pageSize } = pagination
+    const mergeParams = { current, pageSize, ...unref(params) }
+    return fetchList(mergeParams)
+  }
+
+  if (auto) {
+    fetchPageList(params)
+  }
+
+  return { fetchPageList, list, loading, pagination }
+}
